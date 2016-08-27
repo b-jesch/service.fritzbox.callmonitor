7013f2786c91cc29662ec1f7ee7e9d125624e9e0
lutzleonhardt <lutzleonhardt@users.noreply.github.com>
2016-08-27 17:34:36 +0200

    fix exception in iCloud code (occurred on android/python 2.6 systems): AttributeError: 'module' object has no attribute 'NullHandler'
    solves issue #6

--- resources/lib/PhoneBooks/pyicloud/__init__.py
+++ resources/lib/PhoneBooks/pyicloud/__init__.py
@@ -1,4 +1,12 @@
 import logging
 from .base import PyiCloudService
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+#http://stackoverflow.com/questions/33175763/how-to-use-logging-nullhandler-in-python-2-6
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
