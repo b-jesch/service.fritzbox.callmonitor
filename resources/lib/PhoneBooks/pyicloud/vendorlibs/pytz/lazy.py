a7a1794e6be44581b86c67ef83d160f5573f1067
b-jesch <birger.jesch@gmail.com>
2022-11-16 09:10:30 +0100

    fix import error on python 3.11

--- resources/lib/PhoneBooks/pyicloud/vendorlibs/pytz/lazy.py
+++ resources/lib/PhoneBooks/pyicloud/vendorlibs/pytz/lazy.py
@@ -1,9 +1,10 @@
 from threading import RLock
-try:
-    from UserDict import DictMixin
-except ImportError:
-    from collections import Mapping as DictMixin
 
+try:
+    from collections import Mapping as DictMixin
+except ImportError:
+    # Python 3.11+
+    from collections.abc import Mapping as DictMixin
 
 # With lazy loading, we might end up with multiple threads triggering
 # it at the same time. We need a lock.
@@ -13,6 +14,7 @@ _fill_lock = RLock()
 class LazyDict(DictMixin):
     """Dictionary populated on first use."""
     data = None
+
     def __getitem__(self, key):
         if self.data is None:
             _fill_lock.acquire()
@@ -165,4 +167,6 @@ class LazySet(set):
 # Not all versions of Python declare the same magic methods.
 # Filter out properties that don't exist in this version of Python
 # from the list.
+
+
 LazySet._props = [prop for prop in LazySet._props if hasattr(set, prop)]
